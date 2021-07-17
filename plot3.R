87e702b9715217f45fa96a38888463a21392f30f
PiotrTadeuszZalewski <87559457+PiotrTadeuszZalewski@users.noreply.github.com>
2021-07-17 16:15:42 +0400

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,50 @@
+require(readr)
+hpc<-read_delim("household_power_consumption.txt",
+                delim=";",
+                col_names=c("Date",
+                            "Time",
+                            "Global_active_power",
+                            "Global_reactive_power",
+                            "Voltage",
+                            "Global_intensity",
+                            "Sub_metering_1",
+                            "Sub_metering_2",
+                            "Sub_metering_3"),
+                col_types=cols(Date=col_date(format="%d/%m/%Y"),
+                               Time=col_time(format="%H:%M:%S"),
+                               Global_active_power="d",
+                               Global_reactive_power="d",
+                               Voltage="d",
+                               Global_intensity="d",
+                               Sub_metering_1="d",
+                               Sub_metering_2="d",
+                               Sub_metering_3="d"),
+                locale=locale(decimal_mark=".",grouping_mark = ","),
+                na=c("?"),
+                quoted_na=FALSE,
+                skip=66637,
+                n_max=2881)
+#
+png(filename="plot3.png",
+    width=480,
+    height=480)
+#
+par(xaxt="n")
+plot(hpc$Sub_metering_1,type="l",
+     xlab="",
+     ylab="Energy sub metering")
+lines(hpc$Sub_metering_2,
+      col="red")
+lines(hpc$Sub_metering_3
+      ,col="blue")
+par(xaxt="s")
+axis(1,
+     at=c(1,1441,2881),
+     labels=c("Thu","Fri","Sat"))
+legend("topright",
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       col=c("black","red","blue"),
+       lty=1,
+       lwd=1)
+#
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,79 @@
+require(readr)
+hpc<-read_delim("household_power_consumption.txt",
+                delim=";",
+                col_names=c("Date",
+                            "Time",
+                            "Global_active_power",
+                            "Global_reactive_power",
+                            "Voltage",
+                            "Global_intensity",
+                            "Sub_metering_1",
+                            "Sub_metering_2",
+                            "Sub_metering_3"),
+                col_types=cols(Date=col_date(format="%d/%m/%Y"),
+                               Time=col_time(format="%H:%M:%S"),
+                               Global_active_power="d",
+                               Global_reactive_power="d",
+                               Voltage="d",
+                               Global_intensity="d",
+                               Sub_metering_1="d",
+                               Sub_metering_2="d",
+                               Sub_metering_3="d"),
+                locale=locale(decimal_mark=".",grouping_mark = ","),
+                na=c("?"),
+                quoted_na=FALSE,
+                skip=66637,
+                n_max=2881)
+#
+png(filename="plot4.png",
+    width=480,
+    height=480)
+#
+par(mfcol=c(2,2))
+# plot 1
+par(xaxt="n")
+plot(hpc$Global_active_power,type="l",xlab="",ylab="Global Active Power")
+par(xaxt="s")
+axis(1,at=c(1,1441,2881),labels=c("Thu","Fri","Sat"))
+# plot 2
+par(xaxt="n")
+plot(hpc$Sub_metering_1,
+     type="l",
+     xlab="",
+     ylab="Energy sub metering")
+lines(hpc$Sub_metering_2,
+      col="red")
+lines(hpc$Sub_metering_3,
+      col="blue")
+par(xaxt="s")
+axis(1,
+     at=c(1,1441,2881),
+     labels=c("Thu","Fri","Sat"))
+legend("topright",
+        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+        col=c("black","red","blue"),
+        lty=1,
+        lwd=1,
+        bty="n")
+# plot 3
+par(xaxt="n")
+plot(hpc$Voltage,
+     type="l",
+     xlab="datetime",
+     ylab="Voltage")
+par(xaxt="s")
+axis(1,
+     at=c(1,1441,2881),
+     labels=c("Thu","Fri","Sat"))
+# plot 4
+par(xaxt="n")
+plot(hpc$Global_reactive_power,
+     type="l",
+     xlab="datetime",
+     ylab="Global_reactive_power")
+par(xaxt="s")
+axis(1,
+     at=c(1,1441,2881),
+     labels=c("Thu","Fri","Sat"))
+#
+dev.off()
